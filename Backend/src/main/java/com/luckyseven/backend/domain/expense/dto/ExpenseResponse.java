f70adadd1252d5a9b3ea180a25e3e1f05d8ce1f0
pbk2312 <pbk2312@inu.ac.kr>
2025-05-09 16:45:21 +0900

    feat(Expense): Expense 응답용 Dto 작성
    - expenseId
    - amount
    - foreignBalance
    - createdAt
    - updatedAt
    - balance

---
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/dto/ExpenseResponse.java
@@ -0,0 +1,23 @@
+package com.luckyseven.backend.domain.expense.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class ExpenseResponse {
+
+    private Long expenseId;
+    private BigDecimal amount;
+    private BigDecimal foreignBalance;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private BigDecimal balance;
+}
