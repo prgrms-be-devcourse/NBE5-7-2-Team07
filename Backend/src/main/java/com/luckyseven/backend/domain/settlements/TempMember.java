7250ed5fba7ae70be6d8d9a7b0dc5ead07f98a08
KEEKE132 <dksvlfdhs1@gmail.com>
2025-05-09 15:30:38 +0900

    feat: add temporal classes

---
+++ Backend/src/main/java/com/luckyseven/backend/domain/settlements/TempMember.java
@@ -0,0 +1,8 @@
+package com.luckyseven.backend.domain.settlements;
+
+import com.luckyseven.backend.sharedkernel.entity.BaseEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class TempMember extends BaseEntity {
+}
