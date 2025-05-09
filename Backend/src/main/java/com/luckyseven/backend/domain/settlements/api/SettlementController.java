74805db14ce850db387f3e55f778092403e07357
KEEKE132 <dksvlfdhs1@gmail.com>
2025-05-09 16:47:07 +0900

    feat: add Swagger annotation

--- Backend/src/main/java/com/luckyseven/backend/domain/settlements/api/SettlementController.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/settlements/api/SettlementController.java
@@ -3,6 +3,10 @@ package com.luckyseven.backend.domain.settlements.api;
 import com.luckyseven.backend.domain.settlements.app.SettlementService;
 import com.luckyseven.backend.domain.settlements.dto.SettlementResponse;
 import com.luckyseven.backend.domain.settlements.dto.SettlementUpdateRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,15 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "Settlement")
 public class SettlementController {
 
   private final SettlementService settlementService;
 
+  @Operation(summary = "ID로 정산 조회")
+  @ApiResponse(responseCode = "200", description = "Settlement found")
+  @ApiResponse(responseCode = "404", description = "Settlement not found")
   @GetMapping("/settlements/{settlementId}")
   public ResponseEntity<SettlementResponse> readSettlement(@PathVariable Long settlementId) {
     return ResponseEntity.ok(settlementService.readSettlement(settlementId));
   }
 
+  @Operation(summary = "팀ID로 정산 목록 조회, 필터조건 추가 가능")
+  @ApiResponse(responseCode = "200", description = "Successfully retrieved settlements")
+  @ApiResponse(responseCode = "400", description = "TeamID가 null인경우")
   @GetMapping("/teams/{teamId}/settlements")
   public ResponseEntity<Page<SettlementResponse>> readSettlements(
       @PathVariable Long teamId,
@@ -41,9 +52,13 @@ public class SettlementController {
     return ResponseEntity.ok(settlementPage);
   }
 
+  @Operation(summary = "Update settlement")
+  @ApiResponse(responseCode = "200", description = "Settlement updated successfully")
+  @ApiResponse(responseCode = "404", description = "Settlement not found")
   @PatchMapping("/settlements/{settlementId}")
-  public ResponseEntity<SettlementResponse> updateSettlement(@PathVariable Long settlementId,
-      @RequestParam(defaultValue = "false") Boolean settledOnly
+  public ResponseEntity<SettlementResponse> updateSettlement(
+      @PathVariable Long settlementId,
+      @Parameter(description = "true면 body를 무시하고 정산처리만 진행") @RequestParam(defaultValue = "false") Boolean settledOnly
       , @RequestBody SettlementUpdateRequest request) {
     if (settledOnly) {
       return ResponseEntity.ok(settlementService.settleSettlement(settlementId));
