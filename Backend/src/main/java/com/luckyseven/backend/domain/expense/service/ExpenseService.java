2a6ebed4c4a8b43960212e001388ea0897ce1c6c
pbk2312 <pbk2312@inu.ac.kr>
2025-05-09 17:26:54 +0900

    feat(Expense): Todo 작성

--- Backend/src/main/java/com/luckyseven/backend/domain/expense/service/ExpenseService.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/service/ExpenseService.java
@@ -1,5 +1,6 @@
 package com.luckyseven.backend.domain.expense.service;
 
+import com.luckyseven.backend.domain.expense.dto.ExpenseBalanceResponse;
 import com.luckyseven.backend.domain.expense.dto.ExpenseRequest;
 import com.luckyseven.backend.domain.expense.dto.ExpenseResponse;
 import com.luckyseven.backend.domain.expense.dto.ExpenseUpdateRequest;
@@ -21,7 +22,12 @@ public class ExpenseService {
 
     @Transactional
     public ExpenseResponse saveExpense(Long teamId, ExpenseRequest request) {
-        // Todo: 팀/결제자/정산 대상자 검증(추후 팀원들 로직 참고)
+
+        // TODO: 현재 로그인한 사용자가 해당 팀(teamId)의 팀원인지 검증 (팀원 로직 참고)
+        // TODO: 로그인한 사용자 ID를 결제자(payerId)로 설정 (팀원 로직 참고)
+        // TODO: 정산 대상자들이 모두 해당 팀 소속인지 검증 (팀원 로직 참고)
+        // TODO: 예산 원화 잔고 차감
+
         Expense expense = ExpenseMapper.toExpense(request, teamId);
         Expense saved = expenseRepository.save(expense);
         return ExpenseMapper.toExpenseResponse(saved);
@@ -33,10 +39,35 @@ public class ExpenseService {
 
         Expense expense = getExpenseById(expenseId);
 
+        // TODO: 현재 로그인한 사용자의 ID를 SecurityContextHolder 또는 JWT 유틸에서 꺼냄
+        // TODO: 해당 ID가 expense.payerId와 일치하는지 검증
+        // TODO: 예산 원화 잔고 수정
+
         expense.update(request.getDescription(), request.getAmount(), request.getCategory());
 
         return ExpenseMapper.toExpenseResponse(expense);
     }
+
+    @Transactional
+    public ExpenseBalanceResponse deleteExpense(Long expenseId) {
+
+        // TODO: 로그인 사용자 ID와 expense.payerId 일치하는지 검증
+
+        Expense expense = getExpenseById(expenseId);
+
+        Long teamId = expense.getTeamId();
+
+        // TODO: 팀 조회
+        // TODO: budgetId를 팀에서 추출
+        // TODO: 예산 조회
+        // TODO; 예산 원화 잔고 증가
+
+        expenseRepository.delete(expense);
+
+        // TODO: balance 와 foreginBalance 변환하여 ExpenseBalanceResponse 반환
+        return null;
+    }
+
 
     private Expense getExpenseById(Long expenseId) {
         return expenseRepository.findById(expenseId)
