ead735ebef11714bbd20822bda999c7c78ed4efb
pbk2312 <pbk2312@inu.ac.kr>
2025-05-09 14:48:37 +0900

    feat(Expense): 경비 엔티티 및 관련 enum 추가
    - Expense 엔티티 구현
    - ExpenseCategory 구현
    - PaymentMethod 구현

---
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/entity/Expense.java
@@ -0,0 +1,48 @@
+package com.luckyseven.backend.domain.expense.entity;
+
+import com.luckyseven.backend.domain.expense.enums.ExpenseCategory;
+import com.luckyseven.backend.sharedkernel.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Entity
+@Table(
+    indexes = @Index(name = "idx_expense_payer", columnList = "payer_id")
+)
+public class Expense extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "expense_id", nullable = false)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String description;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ExpenseCategory category;
+
+    @Column(name = "payer_id", nullable = false)
+    private Long payerId;
+}
