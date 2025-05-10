6037944648bc0524913ae3cb2c7c77b13ebe2919
pbk2312 <pbk2312@inu.ac.kr>
2025-05-10 13:56:26 +0900

    refactor(expense): Todo 삭제 및 temp 엔티티 추가

--- Backend/src/main/java/com/luckyseven/backend/domain/expense/mapper/ExpenseMapper.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/mapper/ExpenseMapper.java
@@ -1,34 +1,44 @@
 package com.luckyseven.backend.domain.expense.mapper;
 
+import com.luckyseven.backend.domain.expense.dto.CreateExpenseResponse;
+import com.luckyseven.backend.domain.expense.dto.ExpenseBalanceResponse;
 import com.luckyseven.backend.domain.expense.dto.ExpenseRequest;
-import com.luckyseven.backend.domain.expense.dto.ExpenseResponse;
 import com.luckyseven.backend.domain.expense.entity.Expense;
+import com.luckyseven.backend.domain.expense.util.TempBudget;
+import com.luckyseven.backend.domain.expense.util.TempMember;
+import com.luckyseven.backend.domain.expense.util.TempTeam;
 
 public class ExpenseMapper {
 
-    private ExpenseMapper() {
-    }
+  private ExpenseMapper() {
+  }
 
-    public static Expense toExpense(ExpenseRequest request, Long teamId) {
-        return Expense.builder()
-            .description(request.getDescription())
-            .amount(request.getAmount())
-            .category(request.getCategory())
-            // TODO: SecurityContextHolder 에서 payerId 꺼내오도록 구현
-            .payerId(null)
-            .teamId(teamId)
-            .build();
-    }
+  public static Expense toExpense(ExpenseRequest request, TempTeam team, TempMember payer) {
+    return Expense.builder()
+        .description(request.getDescription())
+        .amount(request.getAmount())
+        .category(request.getCategory())
+        .payer(payer)
+        .team(team)
+        .build();
+  }
 
-    public static ExpenseResponse toExpenseResponse(Expense expense) {
-        return ExpenseResponse.builder()
-            .amount(expense.getAmount())
-            .expenseId(expense.getId())
-            // TODO: foreignBalance, balance 추가 작성
-            .foreignBalance(null)
-            .balance(null)
-            .createdAt(expense.getCreatedAt())
-            .updatedAt(expense.getUpdatedAt())
-            .build();
-    }
+  public static CreateExpenseResponse toExpenseResponse(Expense expense, TempBudget budget) {
+    return CreateExpenseResponse.builder()
+        .amount(expense.getAmount())
+        .expenseId(expense.getId())
+        .foreignBalance(budget.getForeignBalance())
+        .balance(budget.getBalance())
+        .createdAt(expense.getCreatedAt())
+        .updatedAt(expense.getUpdatedAt())
+        .build();
+  }
+
+  public static ExpenseBalanceResponse toExpenseBalanceResponce(TempBudget budget) {
+    return ExpenseBalanceResponse
+        .builder()
+        .balance(budget.getBalance())
+        .foreignBalance(budget.getForeignBalance())
+        .build();
+  }
 }
