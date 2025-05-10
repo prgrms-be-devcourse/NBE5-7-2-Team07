3e980741f6fd0da6f90395501476df420401fa49
pbk2312 <pbk2312@inu.ac.kr>
2025-05-10 16:02:02 +0900

    feat(expense): 지출 상세 조회 응답에 필요한 책임자 추가

---
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/dto/SettlerResponse.java
@@ -0,0 +1,18 @@
+package com.luckyseven.backend.domain.expense.dto;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class SettlerResponse {
+
+  private Long id;
+  private String nickname;
+}
