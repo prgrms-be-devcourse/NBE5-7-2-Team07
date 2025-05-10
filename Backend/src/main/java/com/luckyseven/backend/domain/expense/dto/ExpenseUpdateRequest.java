32d20934687d85fa319c1359d74532649da9181d
pbk2312 <pbk2312@inu.ac.kr>
2025-05-10 13:57:17 +0900

    style(expense): 포맷팅 적용

--- Backend/src/main/java/com/luckyseven/backend/domain/expense/dto/ExpenseUpdateRequest.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/dto/ExpenseUpdateRequest.java
@@ -17,13 +17,13 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ExpenseUpdateRequest {
 
-    @NotBlank(message = "설명은 공백일 수 없습니다.")
-    private String description;
+  @NotBlank(message = "설명은 공백일 수 없습니다.")
+  private String description;
 
-    @NotNull(message = "금액은 필수 입력 항목입니다.")
-    @DecimalMin(value = "0.00", message = "금액은 0 이상이어야 합니다.")
-    private BigDecimal amount;
+  @NotNull(message = "금액은 필수 입력 항목입니다.")
+  @DecimalMin(value = "0.00", message = "금액은 0 이상이어야 합니다.")
+  private BigDecimal amount;
 
-    @NotNull(message = "카테고리는 필수 선택 항목입니다.")
-    private ExpenseCategory category;
+  @NotNull(message = "카테고리는 필수 선택 항목입니다.")
+  private ExpenseCategory category;
 }
