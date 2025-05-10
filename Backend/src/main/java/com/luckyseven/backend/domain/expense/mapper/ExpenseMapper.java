988da8586ffc5fa5f00f6efd6b993441bfbd85ad
pbk2312 <pbk2312@inu.ac.kr>
2025-05-10 16:01:05 +0900

    feat(expense): PaymentMethod 추가 작성 및 조회를 위한 Dto 변환 메서드 작성

--- Backend/src/main/java/com/luckyseven/backend/domain/expense/mapper/ExpenseMapper.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/mapper/ExpenseMapper.java
@@ -3,10 +3,14 @@ package com.luckyseven.backend.domain.expense.mapper;
 import com.luckyseven.backend.domain.expense.dto.CreateExpenseResponse;
 import com.luckyseven.backend.domain.expense.dto.ExpenseBalanceResponse;
 import com.luckyseven.backend.domain.expense.dto.ExpenseRequest;
+import com.luckyseven.backend.domain.expense.dto.ExpenseResponse;
+import com.luckyseven.backend.domain.expense.dto.SettlerResponse;
 import com.luckyseven.backend.domain.expense.entity.Expense;
 import com.luckyseven.backend.domain.expense.util.TempBudget;
 import com.luckyseven.backend.domain.expense.util.TempMember;
+import com.luckyseven.backend.domain.expense.util.TempSettlement;
 import com.luckyseven.backend.domain.expense.util.TempTeam;
+import java.util.List;
 
 public class ExpenseMapper {
 
@@ -17,13 +21,14 @@ public class ExpenseMapper {
     return Expense.builder()
         .description(request.getDescription())
         .amount(request.getAmount())
+        .paymentMethod(request.getPaymentMethod())
         .category(request.getCategory())
         .payer(payer)
         .team(team)
         .build();
   }
 
-  public static CreateExpenseResponse toExpenseResponse(Expense expense, TempBudget budget) {
+  public static CreateExpenseResponse toCreateExpenseResponse(Expense expense, TempBudget budget) {
     return CreateExpenseResponse.builder()
         .amount(expense.getAmount())
         .expenseId(expense.getId())
@@ -39,6 +44,28 @@ public class ExpenseMapper {
         .builder()
         .balance(budget.getBalance())
         .foreignBalance(budget.getForeignBalance())
+        .build();
+  }
+
+  public static ExpenseResponse toExpenseResponse(Expense expense,
+      List<TempSettlement> settlements) {
+    List<SettlerResponse> settlerResponses = settlements.stream()
+        .map(ts -> SettlerResponse.builder()
+            .id(ts.getSettler().getId())
+            .nickname(ts.getSettler().getNickname())
+            .build())
+        .toList();
+
+    return ExpenseResponse.builder()
+        .description(expense.getDescription())
+        .amount(expense.getAmount())
+        .category(expense.getCategory())
+        .payerId(expense.getPayer().getId())
+        .payerNickname(expense.getPayer().getNickname())
+        .createdAt(expense.getCreatedAt())
+        .updatedAt(expense.getUpdatedAt())
+        .paymentMethod(expense.getPaymentMethod())
+        .settlers(settlerResponses)
         .build();
   }
 }
