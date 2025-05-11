e3ae9a5832bdc2a0f6cd062ff88450b4c54b013b
JaeoneHeo <jaewonheo666@gmail.com>
2025-05-12 01:35:28 +0900

    [Feat] Team 가입 api에 필요한 MemberRepository & MemberService 임시 구현

---
+++ Backend/src/main/java/com/luckyseven/backend/domain/team/service/TempMemberService.java
@@ -0,0 +1,22 @@
+package com.luckyseven.backend.domain.team.service;
+
+import com.luckyseven.backend.domain.team.entity.Member;
+import com.luckyseven.backend.domain.team.repository.TempMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TempMemberService {
+
+  private final TempMemberRepository tempMemberRepository;
+
+  public Member getCurrentMember() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    String email = authentication.getName();
+    return tempMemberRepository.findByEmail(email)
+        .orElseThrow(() -> new IllegalArgumentException(""));
+  }
+}
