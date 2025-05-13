8c67a8dea0257aae74a9106c787148938652ab78
iseonbin <twinkey0201@gmail.com>
2025-05-13 11:40:50 +0900

    feat(login) : CustomMemberDetailsService 개발 - checkDuplicateNickName -> 닉네임 중복 확인 - checkDuplicateEmail -> 이메일 중복 확인 - checkEqualsPassword -> 비밀번호가 동일한가 확인 - logout -> 로그아웃 로직 -> 현재 있는 refreshToken을 blackList에 저장, Cookie를 삭제( 이게 올바른 삭제인가 고민 ) - loadUserByUsername -> SpringSecurity 로그인 기본 메소드 - loadUserByUserId -> refreshToken에서 파싱한 JWT 값을 추출해서 CustomUserDetails의 객체를 만드는 메소드

---
+++ Backend/src/main/java/com/luckyseven/backend/domain/member/service/CustomMemberDetailsService.java
@@ -0,0 +1,104 @@
+package com.luckyseven.backend.domain.member.service;
+
+import com.luckyseven.backend.domain.member.service.utill.CustomUserDetails;
+import com.luckyseven.backend.domain.member.dto.RegisterMemberRequest;
+import com.luckyseven.backend.domain.member.entity.MemberEntity;
+import com.luckyseven.backend.domain.member.repository.MemberRepository;
+import com.luckyseven.backend.sharedkernel.exception.CustomLogicException;
+import com.luckyseven.backend.sharedkernel.exception.ExceptionCode;
+import com.luckyseven.backend.sharedkernel.jwt.entity.BlackListToken;
+import com.luckyseven.backend.sharedkernel.jwt.repository.BlackListTokenRepository;
+import com.luckyseven.backend.sharedkernel.jwt.utill.JwtTokenizer;
+import jakarta.servlet.http.Cookie;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CustomMemberDetailsService implements UserDetailsService {
+
+  private final MemberRepository memberRepository;
+  private final JwtTokenizer jwtTokenizer;
+  private final BlackListTokenRepository blackListTokenRepository;
+
+  public void checkDuplicateNickName(String nickName){
+    log.info("[CustomMemberDetailsService] checkDuplicateNickName 호출, email = {}", nickName);
+    if(memberRepository.findByNickname(nickName).isPresent()){
+      throw new CustomLogicException(ExceptionCode.MEMBER_NICKNAME_DUPLICATE,(Object) nickName);
+    }
+  }
+
+  public void checkDuplicateEmail(String email){
+    log.info("[CustomMemberDetailsService] checkDuplicateEmail 호출, email = {}", email);
+    if(memberRepository.findByEmail(email).isPresent()){
+      throw new CustomLogicException(ExceptionCode.MEMBER_EMAIL_DUPLICATE,(Object) email);
+    }
+  }
+
+  public void checkEqualsPassword(String password ,String checkPassword){
+    log.info("[CustomMemberDetailsService] checkEqualsPassword 호출, password = {} , checkpassword = {}", password, checkPassword);
+    if(!password.equals(checkPassword)){
+      throw new CustomLogicException(ExceptionCode.MEMBER_PASSWORD_MISMATCH);
+    }
+  }
+
+  public void registerMember(RegisterMemberRequest req, PasswordEncoder passwordEncoder){
+    checkDuplicateEmail(req.email());
+    checkDuplicateNickName(req.nickname());
+    checkEqualsPassword(req.password(), req.checkPassword());
+
+    String encodePassword = passwordEncoder.encode(req.password());
+    //TODO : {Mapper} : 설정
+    MemberEntity newMemberEntity = MemberEntity.builder()
+        .email(req.email())
+        .password(encodePassword)
+        .nickname(req.nickname())
+        .build();
+    memberRepository.save(newMemberEntity);
+  }
+
+  public void logout(
+      String refreshToken,
+      HttpServletResponse resp) {
+
+
+    blackListTokenRepository.save(
+        BlackListToken.builder()
+            .tokenValue(refreshToken)
+            .expirationTime(
+                jwtTokenizer.parseRefreshToken(refreshToken).getExpiration().toInstant())
+            .build()
+    );
+
+    //TODO: 올바른 삭제 방법인가?
+    Cookie expired = new Cookie("refreshToken", null);
+    expired.setPath("/");
+    expired.setMaxAge(0);
+    resp.addCookie(expired);
+  }
+
+
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    MemberEntity memberEntity = memberRepository.findByEmail(email)
+        .orElseThrow(() -> new CustomLogicException(ExceptionCode.MEMBER_EMAIL_NOTFOUND,email));
+    return new CustomUserDetails(memberEntity);
+  }
+  public CustomUserDetails loadUserById(Long id){
+    MemberEntity memberEntity = memberRepository.findById(id)
+        .orElseThrow(() -> new CustomLogicException(ExceptionCode.MEMBER_ID_NOTFOUND,id));
+    return new CustomUserDetails(memberEntity);
+  }
+
+}
