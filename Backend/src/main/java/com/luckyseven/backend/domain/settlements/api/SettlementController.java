2f36f935879b110f240f96cc8818dcf89beb76d5
KEEKE132 <dksvlfdhs1@gmail.com>
2025-05-13 09:28:08 +0900

    docs: apply ParameterObject

--- Backend/src/main/java/com/luckyseven/backend/domain/settlements/api/SettlementController.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/settlements/api/SettlementController.java
@@ -10,12 +10,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,7 +45,7 @@ public class SettlementController {
   @GetMapping("/teams/{teamId}/settlements")
   public ResponseEntity<Page<SettlementResponse>> readSettlements(
       @PathVariable Long teamId,
-      @ModelAttribute SettlementSearchCondition condition,
+      @ParameterObject SettlementSearchCondition condition,
       @PageableDefault(page = 0, size = 10) Pageable pageable
   ) {
     Page<SettlementResponse> settlementPage = settlementService.readSettlementPage(teamId,
