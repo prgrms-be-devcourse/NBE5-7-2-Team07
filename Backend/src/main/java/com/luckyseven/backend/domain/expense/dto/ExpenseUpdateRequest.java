cf0d001764801cc752a5da22b0ce1887a5aedb77
pbk2312 <pbk2312@inu.ac.kr>
2025-05-13 22:32:32 +0900

    refactor(expense): `PATCH` 요청에 맞게  `ExpenseUpdateRequest`에서 필수 필드 검증 제거

--- Backend/src/main/java/com/luckyseven/backend/domain/expense/dto/ExpenseUpdateRequest.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/dto/ExpenseUpdateRequest.java
@@ -2,8 +2,6 @@ package com.luckyseven.backend.domain.expense.dto;
 
 import com.luckyseven.backend.domain.expense.enums.ExpenseCategory;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -17,13 +15,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExpenseUpdateRequest {
 
-  @NotBlank(message = "설명은 공백일 수 없습니다.")
   private String description;
 
-  @NotNull(message = "금액은 필수 입력 항목입니다.")
   @DecimalMin(value = "0.00", message = "금액은 0 이상이어야 합니다.")
   private BigDecimal amount;
 
-  @NotNull(message = "카테고리는 필수 선택 항목입니다.")
   private ExpenseCategory category;
 }
