f5c49b64338953d751d38f92cc14aa1a3382805f
iseonbin <twinkey0201@gmail.com>
2025-05-13 17:45:44 +0900

    refactor(login) : 중복 코드를 삭제하였습니다

--- Backend/src/main/java/com/luckyseven/backend/core/SecurityConfig.java
+++ Backend/src/main/java/com/luckyseven/backend/core/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.luckyseven.backend.core;
 
-import com.luckyseven.backend.domain.member.service.CustomMemberDetailsService;
+
 import com.luckyseven.backend.sharedkernel.jwt.utill.JwtTokenizer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,8 +31,9 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenizer jwtTokenizer) throws Exception {
     http
+        .cors(Customizer.withDefaults())
         .csrf(csrf -> csrf.disable())
-        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .exceptionHandling(ex -> ex.authenticationEntryPoint(customAuthenticationEntryPoint))
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/api/users/**",
@@ -41,7 +43,6 @@ public class SecurityConfig {
                 "/swagger-resources/**",
                 "/webjars/**","/refresh").permitAll()
             .anyRequest().authenticated())
-        .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(new JwtAuthenticationFilter(jwtTokenizer,customAuthenticationEntryPoint),
             UsernamePasswordAuthenticationFilter.class);
 
