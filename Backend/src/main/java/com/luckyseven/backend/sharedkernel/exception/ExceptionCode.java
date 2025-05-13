785c8de8b437dc33ffe068561ba6c716c507bffa
JaeoneHeo <jaewonheo666@gmail.com>
2025-05-13 15:48:29 +0900

    Refactor: 삭제 로직에서 제한 조건 추가, Team 엔티티에서 명시적 삭제 추가

--- Backend/src/main/java/com/luckyseven/backend/sharedkernel/exception/ExceptionCode.java
+++ Backend/src/main/java/com/luckyseven/backend/sharedkernel/exception/ExceptionCode.java
@@ -43,6 +43,7 @@ public enum ExceptionCode {
   INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다."),
   API_CALL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "API 호출에 실패했습니다.");
 
+
   private final HttpStatus httpStatus;
   private final String message;
 
