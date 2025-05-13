4611b51f04531778b98b57af00a8aef65d995a09
pbk2312 <pbk2312@inu.ac.kr>
2025-05-13 22:37:02 +0900

    refactor(expense): 불필요한 `JPQL` 삭제

--- Backend/src/main/java/com/luckyseven/backend/domain/expense/repository/ExpenseRepository.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/repository/ExpenseRepository.java
@@ -4,15 +4,8 @@ import com.luckyseven.backend.domain.expense.entity.Expense;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
-  @Query("""
-        select e
-          from Expense e
-          where e.team.id = :teamId
-      """)
-  Page<Expense> findByTeamId(@Param("teamId") Long teamId, Pageable pageable);
+  Page<Expense> findByTeamId(Long teamId, Pageable pageable);
 }
