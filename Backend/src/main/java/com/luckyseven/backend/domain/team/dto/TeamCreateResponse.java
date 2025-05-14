f42527278ca655844914418c41ca2ff7416aaeb8
JaeoneHeo <jaewonheo666@gmail.com>
2025-05-14 12:09:45 +0900

    Refactor(team): response에서 필요 없는 password 필드 제거

--- Backend/src/main/java/com/luckyseven/backend/domain/team/dto/TeamCreateResponse.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/team/dto/TeamCreateResponse.java
@@ -1,6 +1,5 @@
 package com.luckyseven.backend.domain.team.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,7 +13,4 @@ public class TeamCreateResponse {
   private String name;
   private String teamCode;
   private Long leaderId;
-
-  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-  private String teamPassword;
 }
