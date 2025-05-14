66d0939034c659b422a7b8a72ec763926fd4ebdd
iseonbin <twinkey0201@gmail.com>
2025-05-14 14:32:23 +0900

    refactor(login) : 필요없는 주입을 삭제 import를 정리하였습니다.

--- Backend/src/main/java/com/luckyseven/backend/sharedkernel/jwt/Controller/RefreshTokenController.java
+++ Backend/src/main/java/com/luckyseven/backend/sharedkernel/jwt/Controller/RefreshTokenController.java
@@ -1,13 +1,11 @@
 package com.luckyseven.backend.sharedkernel.jwt.Controller;
 
-import com.luckyseven.backend.domain.member.service.CustomMemberDetailsService;
-import com.luckyseven.backend.domain.member.service.utill.CustomUserDetails;
+
 import com.luckyseven.backend.sharedkernel.jwt.utill.JwtTokenizer;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RefreshTokenController {
   private final JwtTokenizer jwtTokenizer;
-  private final CustomMemberDetailsService customMemberDetailsService;
   @PostMapping("/refresh")
   public ResponseEntity<Void> refreshToken(@CookieValue(name = "refreshToken") String token,HttpServletResponse response) {
     String accessToken = jwtTokenizer.validateRefreshToken(token,response);
