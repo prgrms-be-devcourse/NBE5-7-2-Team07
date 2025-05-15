4c5235264c4313cd56d0066bc99bbad52a94f6ad
KEEKE132 <dksvlfdhs1@gmail.com>
2025-05-15 17:21:51 +0900

    refactor: separate id

--- Backend/src/main/java/com/luckyseven/backend/domain/settlements/entity/Settlement.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/settlements/entity/Settlement.java
@@ -8,6 +8,9 @@ import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -29,6 +32,10 @@ import lombok.NoArgsConstructor;
     }
 )
 public class Settlement extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   @Column(nullable = false, precision = 15, scale = 2)
   private BigDecimal amount;
