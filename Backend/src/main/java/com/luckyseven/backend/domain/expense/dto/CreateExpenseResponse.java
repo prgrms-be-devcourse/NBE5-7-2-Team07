9c5d6590ee67c03a85ef250014329590f2ca8ef6
pbk2312 <pbk2312@inu.ac.kr>
2025-05-15 11:46:45 +0900

    refactor(expense): DTO 클래스에서 레코드(record) 타입으로 변경

--- Backend/src/main/java/com/luckyseven/backend/domain/expense/dto/CreateExpenseResponse.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/dto/CreateExpenseResponse.java
@@ -2,20 +2,16 @@ package com.luckyseven.backend.domain.expense.dto;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class CreateExpenseResponse {
+public record CreateExpenseResponse(
+    Long expenseId,
+    BigDecimal amount,
+    BigDecimal foreignBalance,
+    BigDecimal balance,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt
+) {
 
-  private Long expenseId;
-  private BigDecimal amount;
-  private BigDecimal foreignBalance;
-  private BigDecimal balance;
-  private LocalDateTime createdAt;
-  private LocalDateTime updatedAt;
 }
--- Backend/src/main/java/com/luckyseven/backend/domain/expense/dto/ExpenseResponse.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/expense/dto/ExpenseResponse.java
@@ -4,23 +4,19 @@ import com.luckyseven.backend.domain.expense.enums.ExpenseCategory;
 import com.luckyseven.backend.domain.expense.enums.PaymentMethod;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class ExpenseResponse {
+public record ExpenseResponse(
+    Long id,
+    String description,
+    BigDecimal amount,
+    ExpenseCategory category,
+    Long payerId,
+    String payerNickname,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt,
+    PaymentMethod paymentMethod
+) {
 
-  private Long id;
-  private String description;
-  private BigDecimal amount;
-  private ExpenseCategory category;
-  private Long payerId;
-  private String payerNickname;
-  private LocalDateTime createdAt;
-  private LocalDateTime updatedAt;
-  private PaymentMethod paymentMethod;
 }
