f6e05f93080493f7ae64d66e025ed28f0a7091ba
pbk2312 <pbk2312@inu.ac.kr>
2025-05-15 14:34:42 +0900

    refactor(expense): TEMP 엔티티 제거

--- Backend/src/main/java/com/luckyseven/backend/domain/expense/util/TempBudgetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luckyseven.backend.domain.expense.util;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TempBudgetRepository extends JpaRepository<TempBudget, Long> {
-
-}
