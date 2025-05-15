804d7de28d2c669273c8d70c62640ae8d44a0119
KEEKE132 <dksvlfdhs1@gmail.com>
2025-05-15 11:12:32 +0900

    refactor: apply `@ResponseStatus`

--- Backend/src/main/java/com/luckyseven/backend/domain/settlements/api/SettlementController.java
+++ Backend/src/main/java/com/luckyseven/backend/domain/settlements/api/SettlementController.java
@@ -14,13 +14,14 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,36 +36,39 @@ public class SettlementController {
   @ApiResponse(responseCode = "200", description = "Settlement found")
   @ApiResponse(responseCode = "404", description = "Settlement not found")
   @GetMapping("/settlements/{settlementId}")
-  public ResponseEntity<SettlementResponse> readSettlement(@PathVariable Long settlementId) {
-    return ResponseEntity.ok(settlementService.readSettlement(settlementId));
+  @ResponseStatus(HttpStatus.OK)
+  public SettlementResponse readSettlement(@PathVariable Long settlementId) {
+    return settlementService.readSettlement(settlementId);
   }
 
   @Operation(summary = "팀ID로 정산 목록 조회, 필터조건 추가 가능")
   @ApiResponse(responseCode = "200", description = "Successfully retrieved settlements")
   @ApiResponse(responseCode = "400", description = "TeamID가 null인경우")
   @GetMapping("/teams/{teamId}/settlements")
-  public ResponseEntity<Page<SettlementResponse>> readSettlements(
+  @ResponseStatus(HttpStatus.OK)
+  public Page<SettlementResponse> readSettlements(
       @PathVariable Long teamId,
       @ParameterObject SettlementSearchCondition condition,
       @PageableDefault(page = 0, size = 10) Pageable pageable
   ) {
     Page<SettlementResponse> settlementPage = settlementService.readSettlementPage(teamId,
         condition, pageable);
-    return ResponseEntity.ok(settlementPage);
+    return settlementPage;
   }
 
   @Operation(summary = "Update settlement")
   @ApiResponse(responseCode = "200", description = "Settlement updated successfully")
   @ApiResponse(responseCode = "404", description = "Settlement not found")
   @PatchMapping("/settlements/{settlementId}")
-  public ResponseEntity<SettlementResponse> updateSettlement(
+  @ResponseStatus(HttpStatus.OK)
+  public SettlementResponse updateSettlement(
       @PathVariable Long settlementId,
       @Parameter(description = "true면 body를 무시하고 정산처리만 진행") @RequestParam(defaultValue = "false") Boolean settledOnly
       , @Valid @RequestBody SettlementUpdateRequest request) {
     if (settledOnly) {
-      return ResponseEntity.ok(settlementService.settleSettlement(settlementId));
+      return settlementService.settleSettlement(settlementId);
     } else {
-      return ResponseEntity.ok(settlementService.updateSettlement(settlementId, request));
+      return settlementService.updateSettlement(settlementId, request);
     }
   }
 }
