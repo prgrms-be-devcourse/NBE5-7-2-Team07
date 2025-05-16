f2ced22df29190f4c774e03c41692e0060c088ee
iseonbin <twinkey0201@gmail.com>
2025-05-16 10:27:11 +0900

    refactor(Front-settlement) : Settlement합치기

---
+++ Frontend/luckeyseven/src/router/Router.js
@@ -0,0 +1,18 @@
+import { Route,Routes } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Login/Signup";
+
+export default function Router(){
+    return(
+        <Routes>
+            <Route path = "/login" element = {<Login />}></Route>
+            <Route path = "/Signup" element = {<Signup />}></Route>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/teams/:teamId/settlements" element={<TeamSettlementsPage />} />
+            <Route path="/settlements/new" element={<SettlementNewPage />} />
+            <Route path="/settlements/:settlementId" element={<SettlementDetailPage />} />
+            <Route path="/settlements/:settlementId/edit" element={<SettlementEditPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    )
+}
